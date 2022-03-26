cac49d99e7b49af77be4daaccc82a6884b9b97f4
Kenny Drewry <kdrewry@umass.edu>
2022-03-26 14:39:04 -0400

    Post guide button prints to console

--- js/create_guide.js
+++ js/create_guide.js
@@ -1,1 +1,16 @@
 const guide = document.getElementById("create_guide");
+const postButton = document.getElementById("cg_postbutton");
+const title = document.getElementById("cg_title");
+const desc = document.getElementById("cg_desc");
+const inst = document.getElementById("cg_inst");
+const img = document.getElementById("cg_image");
+
+postButton.addEventListener("click", post);
+
+
+function post() {
+    console.log("Title: " + title.value);
+    console.log("Description: " + desc.value);
+    console.log("Instructions: " + inst.value);
+    console.log("Image: " + img.value);
+};
