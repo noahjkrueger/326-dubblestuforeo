d78a776e6fe868922fde573739a1b016ae8e5b35
piyushm2001 <pmakkapati@umass.edu>
2022-04-18 17:06:41 -0400

    check valid email

--- js/login.js
+++ js/login.js
@@ -11,6 +11,7 @@ let notice = document.getElementById('notice');
 let checkBox1 = document.getElementById('checkbox1');
 let checkBox2 = document.getElementById('checkbox2');
 let checkBox3 = document.getElementById('checkbox3');
+let checkButton = document.getElementById('checkBtn');
 
 // userLogin.addEventListener('input', function(){
 //     const uid = guzzzleAPI.login(userLogin.value, passLogin.value);
@@ -57,10 +58,6 @@ checkBox3.addEventListener("click", function visible() {
     }
 });
 
-function validEmail() {
-    
-}
-
 logBtn.addEventListener("click", function() {
     const uid = guzzzleAPI.login(userLogin.value, passLogin.value);
     window.localStorage.setItem("uid", uid); 
@@ -69,12 +66,23 @@ logBtn.addEventListener("click", function() {
 
 let signBtn = document.getElementById('signup');
 signBtn.addEventListener("click", function() {
-    if (passSignup.value === confirmP.value) {
+    let check = email.value;
+    let emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(emailPattern.test(check) && (passSignup.value === confirmP.value)) {
         notice.innerText = "";
-        const uid = guzzzleAPI.createUser(userSignup.value, passSignup.value, "https://i.guim.co.uk/img/media/a1b7129c950433c9919f5670c92ef83aa1c682d9/55_344_1971_1183/master/1971.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=88ba2531f114b9b58b9cb2d8e723abe1", "");    
+        const uid = guzzzleAPI.createUser(userSignup.value, passSignup.value, "https://i.guim.co.uk/img/media/a1b7129c950433c9919f5670c92ef83aa1c682d9/55_344_1971_1183/master/1971.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=88ba2531f114b9b58b9cb2d8e723abe1", "");      
+        alert("User has been created!");
+        return true;
     } else {
         notice.innerText = "*passwords must match*";
+        alert("Retry with a valid Email");
+        return false;
     }
-    console.log("Email: " + email.value + "Username: " + userSignup.value + "Password: " + passSignup.value+ "Confirm Password: " + confirmP.value);
 });
 
+// if (passSignup.value === confirmP.value) {
+//     notice.innerText = "";
+//     const uid = guzzzleAPI.createUser(userSignup.value, passSignup.value, "https://i.guim.co.uk/img/media/a1b7129c950433c9919f5670c92ef83aa1c682d9/55_344_1971_1183/master/1971.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=88ba2531f114b9b58b9cb2d8e723abe1", "");    
+// } else {
+//     notice.innerText = "*passwords must match*";
+// }
