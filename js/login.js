d40a4901eb747dd3f465a57527d06066f34d704c
piyushm2001 <pmakkapati@umass.edu>
2022-04-17 21:03:03 -0400

    login/sinup

--- js/login.js
+++ js/login.js
@@ -1,25 +1,29 @@
 import * as guzzzleAPI from './guzzzle-api.js'
 
-let logBtn = document.getElementById("login");
-let userLogin = document.getElementById("Username").value;
-let passLogin = document.getElementById("password").value;
-let email = document.getElementById("email").value;
-let userSignup = document.getElementById("UsernameSignup").value;
-let passSignup = document.getElementById("passwordSignup").value;
-let confirmP = document.getElementById("confirm_Password").value;
+let logBtn = document.getElementById('login');
+let userLogin = document.getElementById('Username');
+let passLogin = document.getElementById('password');
+let email = document.getElementById("email");
+let userSignup = document.getElementById('UsernameSignup');
+let passSignup = document.getElementById('passwordSignup');
+let confirmP = document.getElementById('confirm_Password');
+let notice = document.getElementById('notice');
 
-console.log(userLogin);
+
 logBtn.addEventListener("click", function() {
     const uid = guzzzleAPI.login(userLogin, passLogin);
-    console.log("Username: " + userLogin + "Password: " + passLogin)
+    console.log("Username: " + userLogin.value + "Password: " + passLogin.value);
 });
 
-let signBtn = document.getElementById("signup");
+let signBtn = document.getElementById('signup');
 signBtn.addEventListener("click", function() {
-    if (passSignup === confirmP) {
-        const uid = guzzzleAPI.createUser(userSignup, passSignup, "", email);
-        window.localStorage.setItem("uid", uid);
+    if (passSignup.value === confirmP.value) {
+        notice.innerText = "";
+        const uid = guzzzleAPI.createUser(userSignup.value, passSignup.value, "https://i.guim.co.uk/img/media/a1b7129c950433c9919f5670c92ef83aa1c682d9/55_344_1971_1183/master/1971.jpg?width=1200&height=900&quality=85&auto=format&fit=crop&s=88ba2531f114b9b58b9cb2d8e723abe1", "");
+        window.localStorage.setItem("uid", uid); // ask noah about this
+    } else {
+        notice.innerText = "*passwords must match*";
     }
-    console.log("Email: " + email + "Username: " + userSignup + "Password: " + passSignup + "Confirm Password: " + confirmP)
+    console.log("Email: " + email.value + "Username: " + userSignup.value + "Password: " + passSignup.value+ "Confirm Password: " + confirmP.value);
 });
 
---
+++ js/profile.js
@@ -0,0 +1,3 @@
+import renderFeed from './feed.js'
+
+
