ab0feea70dc69978b569ef58a01c8f0e0f48fd8c
Kenny Drewry <kdrewry@umass.edu>
2022-04-19 19:14:13 -0400

    Added brand text box

--- js/create_guide.js
+++ js/create_guide.js
@@ -4,14 +4,12 @@ let ingredientCount = 1;
 const ingType = ['Booze', 'Juice', 'Garnish', 'Veggies', 'Fruit'];
 window.localStorage.setItem("uid", "noah");
 
-const guide = document.getElementById("create_guide");
 const postButton = document.getElementById("cg_postbutton");
 const title = document.getElementById("cg_title");
 const desc = document.getElementById("cg_desc");
 const inst = document.getElementById("cg_inst");
 const img = document.getElementById("cg_image");
 const addIng = document.getElementById("cg_btn");
-const ingredients = document.getElementsByClassName("cg_ingredients");
 
 postButton.addEventListener("click", post);
 addIng.addEventListener("click", duplicate);
@@ -22,23 +20,29 @@ function post() {
     let ingredientStr = [];
     for(let i = 1; i <= ingredientCount; ++i) {
         let ing = document.getElementById("cg_ing" + i);
-        let type = document.getElementById("cg_type" + i);
         let amount = document.getElementById("cg_amount" + i);
+        let brand = document.getElementById("cg_brand" + i);
         ingredient_keys.push(ing.value);
-        ingredientStr.push("Ingredient " + i + ": " + ing.value + " " + amount.value + "; ");
+        ingredientStr.push(brand.value + " " + ing.value + " " + amount.value);
     }
              //createPost(uid, title, image, ingredient_keys, ingredients, instructions, description)
     guzzzleAPI.createPost(window.localStorage.getItem("uid"), title.value, img.value, ingredient_keys, ingredientStr, inst.value, desc.value);
 };
 
 function duplicate() {
-    //cg_type1
+    //cg_type
     let originalType = document.getElementById('cg_type' + ingredientCount);
     let cloneType = originalType.cloneNode(true);
     cloneType.id = "cg_type" + (ingredientCount + 1);
     cloneType.value = ''
     originalType.parentNode.appendChild(cloneType);
-    //cg_cg_amount1
+    //cg_brand
+    let originalBrand = document.getElementById('cg_brand' + ingredientCount);
+    let cloneBrand = originalBrand.cloneNode(true);
+    cloneBrand.id = "cg_brand" + (ingredientCount + 1);
+    cloneBrand.value = ''
+    originalBrand.parentNode.appendChild(cloneBrand);
+    //cg_amount
     let originalAmount = document.getElementById('cg_amount' + ingredientCount);
     let cloneAmount = originalAmount.cloneNode(true);
     cloneAmount.id = "cg_amount" + (ingredientCount + 1);
